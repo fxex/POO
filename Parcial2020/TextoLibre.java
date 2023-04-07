327a5fdb600be998c5877fef31a9d95ee67d727f
fxex <hugoantoniofrey2003@gmail.com>
2023-04-07 14:49:23 -0300

    Clase hija2

---
+++ Parcial2020/TextoLibre.java
@@ -0,0 +1,24 @@
+package Parcial2020;
+
+public class TextoLibre extends Pregunta{
+    private String respuesta;
+    public TextoLibre(){
+        super();
+        this.respuesta = "";
+    }
+
+    public TextoLibre(String enun, int num, String res){
+        super(enun, num);
+        this.respuesta = res;
+    }
+
+
+    public String getRespuesta() {
+        return this.respuesta;
+    }
+
+    public void setRespuesta(String respuesta) {
+        this.respuesta = respuesta;
+    }
+
+}

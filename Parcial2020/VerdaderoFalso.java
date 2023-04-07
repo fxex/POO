e882a10b5b460d345ba03333e7503771b8d1d5ba
fxex <hugoantoniofrey2003@gmail.com>
2023-04-07 14:36:49 -0300

    Clase hija1

---
+++ Parcial2020/VerdaderoFalso.java
@@ -0,0 +1,29 @@
+package Parcial2020;
+
+public class VerdaderoFalso extends Pregunta{
+    private boolean verdadero;
+
+    public VerdaderoFalso(){
+        super();
+        verdadero = false;
+    }
+
+    public VerdaderoFalso(String enun, int num, boolean ver){
+        super(enun, num);
+        verdadero = ver;
+    }
+
+
+    public boolean isVerdadero() {
+        return this.verdadero;
+    }
+
+    public boolean getVerdadero() {
+        return this.verdadero;
+    }
+
+    public void setVerdadero(boolean verdadero) {
+        this.verdadero = verdadero;
+    }
+
+}

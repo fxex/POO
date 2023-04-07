27dfcb8f548375fa0d6d1b2a0bb680addd9bde4d
fxex <hugoantoniofrey2003@gmail.com>
2023-04-07 14:09:42 -0300

    Clase base

---
+++ Parcial2020/Pregunta.java
@@ -0,0 +1,35 @@
+package Parcial2020;
+
+public class Pregunta{
+    private String enunciado;
+    private int numero;
+
+    public Pregunta(){
+        this.enunciado = "";
+        this.numero = 0;
+    }
+
+    public Pregunta(String enunciado, int num){
+        this.enunciado = enunciado;
+        this.numero = num;
+    }
+
+
+    public String getEnunciado() {
+        return this.enunciado;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    
+}

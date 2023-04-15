069b4c9ee9af19bb4ca6afeafa714f0c044086fd
fxex <hugoantoniofrey2003@gmail.com>
2023-04-15 04:44:34 -0300

    Creacion de la clase POJO Document

--- Impresora/Document.java
+++ Impresora/Document.java
@@ -1,5 +1,34 @@
 package Impresora;
 
 public class Document {
+    private String nombre;
+    private int cantidadHojas;
     
+    public Document(){
+        this.nombre = "";
+        this.cantidadHojas = 0;
+    }
+
+    public Document(String nombre, int cantHojas){
+        this.nombre = nombre;
+        this.cantidadHojas = cantHojas;
+    }
+
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidadHojas() {
+        return this.cantidadHojas;
+    }
+
+    public void setCantidadHojas(int cantidadHojas) {
+        this.cantidadHojas = cantidadHojas;
+    }
+
 }
